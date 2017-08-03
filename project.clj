76c0f834ab7dd95c21b8b476e5d4833abb84159e
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-08-03 10:37:22 +0200

    [DEPS] bump deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject webhdfs-clj "0.1.0"
+(defproject webhdfs-clj "0.1.1"
             :jvm-opts [;"-Dsun.security.krb5.debug=true"
                        "-Djava.security.auth.login.config=conf/headless.conf"]
             :resource-paths ["conf"]
             :description "Clojure client library for WebHDFS Rest API"
-            :dependencies [[org.clojars.rdsr/clj-http-lite "0.2.1-SNAPSHOT"]
+            :dependencies [[clj-http-lite "0.3.0"]
                            [org.clojure/data.json "0.2.5"]
                            [org.clojure/tools.logging "0.3.1"]
                            [org.clojure/clojure "1.6.0"]])

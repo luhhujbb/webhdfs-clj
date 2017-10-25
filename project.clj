3be26b183938f695252bb3166a9724c09f796364
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-10-25 12:53:16 +0200

    update namespace

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject webhdfs-clj "0.1.3"
+(defproject luhhujbb/webhdfs-clj "0.1.3"
             :jvm-opts [;"-Dsun.security.krb5.debug=true"
                        "-Djava.security.auth.login.config=conf/headless.conf"]
             :resource-paths ["conf"]
             :description "Clojure client library for WebHDFS Rest API"
+            :url "https://github.com/luhhujbb/webhdfs-clj"
+            :license {:name "MIT License"
+                      :url "https://opensource.org/licenses/mit-license.php"}
             :dependencies [[clj-http-lite "0.3.0"]
                            [org.clojure/data.json "0.2.5"]
                            [org.clojure/tools.logging "0.3.1"]
